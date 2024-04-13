a6c5cd77657f296e9744eef7316323539751b430
Orlando Verdin <git@orli.land>
2024-04-13 11:51:42 -0700

    fix problem the size and structure of inputs

--- src/components/Form/index.jsx
+++ src/components/Form/index.jsx
@@ -1,26 +1,23 @@
 import "./form.css";
 
 import Button from "../Button";
-import Input from "../Input";
+import {
+  CardholderInput,
+  CardNumberInput,
+  CardDateInput,
+  CvcInput,
+} from "../Input";
 
 function Form() {
   return (
     <form className="form">
-      <Input
-        label="CARDHOLDER NAME"
-        placeholder="e.g. Jane Appleseed"
-        type="name"
-      />
-      <Input
-        label="CARD NUMBER"
-        placeholder="e.g. 1234 5678 9123 0000"
-        type="card"
-      />
+      <CardholderInput />
+      <CardNumberInput />
       <div className="form__group">
-        <Input label="Exp. Date (MM/YY)" placeholder="MM" type="month" />
-        <Input placeholder="YY" type="year" />
-        <Input label="CVC" placeholder="e.g. 123" type="cvc" />
+        <CardDateInput />
+        <CvcInput />
       </div>
+
       <Button>Confirm</Button>
     </form>
   );
