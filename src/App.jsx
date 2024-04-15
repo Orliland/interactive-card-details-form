b4a66c82007f991c5571af8e00c20c557e1f709f
Orlando Verdin <git@orli.land>
2024-04-14 19:32:41 -0700

    add basic logic for form or modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { useState } from "react";
 import "./App.css";
 
 import Form from "./components/Form";
+import Modal from "./components/Modal";
 import { FrontCard, BackCard } from "./components/Card";
 
 function App() {
+  const [formValidated, setFormValidated] = useState(true);
+
   return (
     <main className="main">
       <section className="section section--cards">
@@ -13,7 +17,7 @@ function App() {
         </div>
       </section>
       <section className="section section--body">
-        <Form />
+        {formValidated ? <Modal /> : <Form />}
       </section>
     </main>
   );
